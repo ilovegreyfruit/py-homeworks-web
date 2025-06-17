8b42252bb2bb8cbd95653c984f07856276bf53bd
Nikita Schwarz <ilovegreyfruit@yandex.ru>
2025-06-17 19:23:09 +0300

    1.3 homework done

---
+++ 1.3-docker/3.2-crud/stocks_products/logistic/serializers.py
@@ -0,0 +1,57 @@
+from email.policy import default
+from itertools import product
+
+from rest_framework import serializers
+from .models import Product, Stock, StockProduct
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id', 'title', 'description']
+
+class ProductPositionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = StockProduct
+        fields = ['product', 'quantity', 'price']
+
+class StockSerializer(serializers.ModelSerializer):
+    positions = ProductPositionSerializer(many=True)
+
+    class Meta:
+        model = Stock
+        fields = ['id', 'address', 'positions']
+
+    def create(self, validated_data):
+        # достаем связанные данные для других таблиц
+        positions = validated_data.pop('positions')
+
+        # создаем склад по его параметрам
+        stock = super().create(validated_data)
+
+        for position in positions:
+            StockProduct.objects.create(stock=stock, **position)
+        # в нашем случае: таблицу StockProduct
+        # с помощью списка positions
+
+        return stock
+
+    def update(self, instance, validated_data):
+        # достаем связанные данные для других таблиц
+        positions = validated_data.pop('positions')
+
+        # обновляем склад по его параметрам
+        stock = super().update(instance, validated_data)
+
+        for position in positions:
+            StockProduct.objects.update_or_create(
+                stock=stock,
+                product=position['product'],
+                defaults={
+                    'quantity': position['quantity'],
+                    'price': position['price'],
+                }
+            )
+        # в нашем случае: таблицу StockProduct
+        # с помощью списка positions
+
+        return stock
